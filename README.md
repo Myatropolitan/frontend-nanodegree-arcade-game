e169774776942d89435dc43b64d14436a822a041
Myatropolitan <miaghori@gmail.com>
2015-05-14 14:01:04 +0100

    initial commit.

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ frontend-nanodegree-arcade-game
 Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
 
 for self-checking their submission.
+
+
+**** HOW TO PLAY ****
+The game is started upon loading of the page.
+Use the up, down, left & right arrow to navigate the player across the screen.
+The goal is to get the player across to the top river section, where the player can earn a point, shown on the bottom right of the screen.
+
+The player gets 1 life to start with, however, may increase a life by capturing the heart icon.
+The number of lives a player has is shown on the bottom right of the screen.
+
+When the player collides with the enemy bug, a life is subtracted.
+When the player life reaches 0, the game is reset.
--- js/app.js
+++ js/app.js
@@ -1,7 +1,38 @@
+
+//Set values
+
+//canvas related
+var canvasWidth = 505;
+var rowHeight = 83;
+var columnWidth = 101;
+//the Y distance between enemies
+//(since setting to rowHeight does not look pretty)
+var enemySepY = 70;
+
+//minimum and maximum speed range for enemies
+var speedMin = 200;
+var speedMax = 400;
+
+//initial starting position of the player
+var initialPlayerY = 390;
+var initialPlayerX = columnWidth * 2;
+
+//game score
+var score = 0;
+
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+
+    //set random speed
+    this.speed = getRandomInt(speedMin, speedMax);
+    //set start position x from outside of canvas, to before going off canvas
+    this.x = getRandomInt((columnWidth * -1), 404);
+    //set start position y on one of the three rows of stone
+    this.y = getRandomInt(1,3) * enemySepY;
+    this.ignoreCollision = false;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +45,44 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    //next intended x position
+    var nextLoc = this.x + (this.speed * dt);
+
+    //reset start position & speed if enemy goes off canvas
+    if(nextLoc > canvasWidth){
+        this.x = (columnWidth * -1);
+        this.y = getRandomInt(1,3) * enemySepY;
+        this.speed = getRandomInt(speedMin, speedMax);
+        this.ignoreCollision = false;
+    //otherwise move the enemy forward
+    }else{
+        this.x = nextLoc;
+    }
+
+    //enemy and player collision detection.
+    //make characters overlap a little for collision effect.
+    if(player.x < (this.x + columnWidth/2) &&
+        player.x + columnWidth/2 > this.x &&
+        player.y < (this.y + rowHeight/2) &&
+        (player.y + rowHeight/2) > this.y){
+
+        //remove a life from player if it is the first time
+        //colliding with that enemy on the particular run across the screen
+        if(this.ignoreCollision == false){
+            player.removeLife();
+
+            //if player is out of lives, reset
+            if(player.lives == 0){
+                resetGame();
+            //if player has lives left after collision, further collision with
+            //that enemy on the particular run is to be ignored
+            }else{
+                this.ignoreCollision = true;
+            }
+        }
+    }
+
 }
 
 // Draw the enemy on the screen, required method for game
@@ -25,11 +94,140 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+// Player of the game
+var Player = function() {
+    //reset player parameters
+    this.resetPlayer();
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+}
+
+//remove a life from the player
+Player.prototype.removeLife = function() {
+    this.lives = this.lives - 1;
+}
+
+//add a life to the player
+Player.prototype.addLife = function() {
+    this.lives = this.lives + 1;
+}
+
+Player.prototype.update = function() {
+    //Add a life if player collides with the heart
+    if(this.x < (heart.x + columnWidth/2) &&
+        this.x + columnWidth/2 > heart.x &&
+        this.y < (heart.y + rowHeight/2) &&
+        (this.y + rowHeight/2) > heart.y){
+        this.addLife();
+    }
+}
+
+// Draw the player on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+    ctx.font = "bold 24px sans-serif";
+    //display number of lives left
+    ctx.fillText("Lives: " + this.lives, 420, 570);
+    //display scoring of the game
+    ctx.fillText("Score: " + score, 305, 570);
+}
+
+//Reset player position & lives
+Player.prototype.resetPlayer = function(){
+    this.x = initialPlayerX;
+    this.y = initialPlayerY;
+    this.lives = 1;
+}
+
+Player.prototype.handleInput = function(key) {
+    var newX, newY;
+
+    if(key == 'left'){
+        newX = this.x - columnWidth;
+        //move left if player does not go off canvas
+        if(newX >= 0)
+            this.x = newX;
+    }else if(key == 'up'){
+        newY = this.y - rowHeight;
+        //move up if player has not reached the water
+        if(newY > 0){
+            this.y = newY;
+        //if player has reached the water, add a score and reset to start position
+        }else{
+            score++;
+            this.resetPlayer();
+        }
+    }else if(key == 'right'){
+        newX = this.x + columnWidth;
+        //move right if player does not go off canvas
+        if(newX < canvasWidth)
+            this.x = newX;
+    }else if(key == 'down'){
+        newY = this.y + rowHeight;
+        //move down if player does not go off canvas
+        if(newY < (rowHeight*5))
+            this.y = newY;
+    }
+}
+
+// Heart item
+var Heart = function() {
+    this.resetHeart();
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/Heart.png';
+}
+
+Heart.prototype.update = function() {
+    //collision detection. make characters overlap a little for collision effect.
+    if(player.x < (this.x + columnWidth/2) &&
+        player.x + columnWidth/2 > this.x &&
+        player.y < (this.y + rowHeight/2) &&
+        (player.y + rowHeight/2) > this.y){
+        this.x = -101; //remove from screen (set outside of the displayed canvas)
+    }
+}
+
+// Draw the player on the screen
+Heart.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Heart.prototype.resetHeart = function() {
+    //reset location
+    this.x = getRandomInt(0,4) * columnWidth;
+    this.y = 320;
+}
+
+function resetGame(){
+    //reset player position
+    player.resetPlayer();
+    //reset heart
+    heart.resetHeart();
+    //reset score
+    score = 0;
+}
+
+//Get random whole number between min & max
+function getRandomInt(min, max) {
+  return Math.floor( Math.random() * (max - min + 1) ) + min;
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var enemyNum = 3;
+var allEnemies = [];
+for(i=0; i<enemyNum; i++){
+    allEnemies[i] = new Enemy();
+}
 
+var player = new Player();
+var heart = new Heart();
 
 
 // This listens for key presses and sends the keys to your
